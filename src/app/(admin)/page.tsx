ecde3bd432ccfe6c1bbcd51f5f02f73f73ef7f6c
Sudhanshu Tiwari <tiwarisudhanshu861@gmail.com>
2025-10-22 19:54:08 +0530

    UPDATE 27

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Metadata } from "next";
+
 import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import React, { useEffect, useState } from "react";
 import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
